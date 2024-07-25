49e2033abf0f06fbc3abe9a23b21868432389f7e
andre-mf <andremendesfelix@gmail.com>
2024-07-24 23:16:30 -0300

    How To Find LDAP User Login Details in Spring Boot part -2 | Spring boot ldap Login User details

--- src/main/java/com/example/springldap/controller/LdapAuthenticationController.java
+++ src/main/java/com/example/springldap/controller/LdapAuthenticationController.java
@@ -2,6 +2,8 @@ package com.example.springldap.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +14,15 @@ public class LdapAuthenticationController {
     @GetMapping("/")
     public String index() {
         return "Welcome to the home page!";
+    }
+
+    @GetMapping("/getUserDetails")
+    public String getUserDetails(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        // access user details
+        String userName = userDetails.getUsername();
+        boolean accNonExpired = userDetails.isAccountNonExpired();
+        return "UserDetails: " + userName + "\n Account Non Expired: " + accNonExpired;
     }
 }
