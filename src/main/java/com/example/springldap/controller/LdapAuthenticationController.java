4d7c674aef8ca2387f6c5e10abb0dad3b58a0e99
andre-mf <andremendesfelix@gmail.com>
2024-07-25 14:28:36 -0300

    How to Get All LDAP User in Spring Boot

--- src/main/java/com/example/springldap/controller/LdapAuthenticationController.java
+++ src/main/java/com/example/springldap/controller/LdapAuthenticationController.java
@@ -1,5 +1,7 @@
 package com.example.springldap.controller;
 
+import com.example.springldap.model.LdapUser;
+import com.example.springldap.service.LdapService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
@@ -7,9 +9,16 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class LdapAuthenticationController {
     private static final Logger log = LoggerFactory.getLogger(LdapAuthenticationController.class);
+    private final LdapService ldapService;
+
+    public LdapAuthenticationController(LdapService ldapService) {
+        this.ldapService = ldapService;
+    }
 
     @GetMapping("/")
     public String index() {
@@ -24,5 +33,10 @@ public class LdapAuthenticationController {
         String userName = userDetails.getUsername();
         boolean accNonExpired = userDetails.isAccountNonExpired();
         return "UserDetails: " + userName + "\n Account Non Expired: " + accNonExpired;
+    }
+
+    @GetMapping("/getAllUsers")
+    public List<LdapUser> getAllUsers() {
+        return ldapService.getAllUsers();
     }
 }
